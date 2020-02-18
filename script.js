f06ae539b43adda26d84c36f5875648ccdfc3912
nidhaal12 <jemlinidhal3@gmail.com>
2020-02-18 16:30:04 +0100

    shopping carte

---
+++ script.js
@@ -0,0 +1,47 @@
+var data=["",0,0,0];
+var p=["",80,120,90]
+var t=[]
+ function icrement(i){
+       
+   
+    data[i]++
+    t[i]=p[i]*data[i]
+    T=t.reduce((a,b)=>a+b)
+
+  
+     document.getElementById("boutonac"+i).innerText=data[i]
+     document.getElementById("item-price"+i).innerText=t[i]+"dt"
+     document.getElementById("tot-shop").innerText=T+"dt"
+      
+
+ }
+ function decrement(i){
+    
+        if((data[i]>0) && (p[i]>0))
+        {
+            data[i]--
+            
+           
+        }
+        else data[i]=0
+        t[i]=p[i]*data[i]
+        T=t.reduce((a,b)=>a+b)
+        document.getElementById("tot-shop").innerText=T+"dt"
+        
+        
+      document.getElementById("boutonac"+i).innerText=data[i]
+      document.getElementById("item-price"+i).innerText=t[i]+"dt"
+    
+    }
+var tab=document.querySelectorAll('.shoping-item-delete');
+for(i=0;i<=3;i++)
+{ if( tab[i]){
+   tab[i].addEventListener('click',
+   function(event){
+    // event.Target.parentNode.parentNode.remove()
+   event.target.parentNode.parentNode.remove()
+  
+  
+  })
+}
+  }
